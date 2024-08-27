66aaa195a00f5672bdba93cd7ee7470fcd044c60
Ryan Heuser <ryan.heuser@gmail.com>
2024-08-27 12:18:20 +0100

    fix

--- crossreads_petrography/testing.py
+++ crossreads_petrography/testing.py
@@ -5,10 +5,16 @@ def run_tests(verbosity_str="q"):
     import pytest
     testcmd = [f"-{verbosity_str}", "--disable-warnings", str(TEST_DIR)]
     logger.setLevel(logging.CRITICAL + 1)
+    logger.info(f"Running tests with verbosity: {verbosity_str}")
     try:
         exit_code = pytest.main(testcmd)
         logger.setLevel(logging.INFO)
-        return exit_code == 0
+        if exit_code == 0:
+            logger.info("\nAll tests passed")
+            return True
+        else:
+            logger.error("\nSome tests failed")
+            return False
     except Exception as e:
         logger.setLevel(logging.INFO)
         logger.error(f"Testing error: {e}")
