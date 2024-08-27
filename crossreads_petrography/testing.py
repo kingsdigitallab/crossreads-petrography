3f40d38ee716eb558aeddd9e3c7c608a38bca06f
Ryan Heuser <ryan.heuser@gmail.com>
2024-08-27 12:16:49 +0100

    no logs

--- crossreads_petrography/testing.py
+++ crossreads_petrography/testing.py
@@ -4,10 +4,12 @@ TEST_DIR = Path(__file__).parent.parent / "tests"
 def run_tests(verbosity_str="q"):
     import pytest
     testcmd = [f"-{verbosity_str}", "--disable-warnings", str(TEST_DIR)]
-    logger.setLevel(logging.ERROR)
+    logger.setLevel(logging.CRITICAL + 1)
     try:
         exit_code = pytest.main(testcmd)
+        logger.setLevel(logging.INFO)
         return exit_code == 0
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logger.setLevel(logging.INFO)
+        logger.error(f"Testing error: {e}")
         return False
